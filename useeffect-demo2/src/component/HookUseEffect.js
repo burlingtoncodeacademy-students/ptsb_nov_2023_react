013fafa161579642891138307a1bef79ff9a7c30
u4ik <insighteuphoric@gmail.com>
2024-02-15 20:41:04 -0500

    finished useeffect demo

--- useeffect-demo2/src/component/HookUseEffect.js
+++ useeffect-demo2/src/component/HookUseEffect.js
@@ -44,9 +44,69 @@ const HookUseEffect = () => {
 
   return (
     <div>
-      <h3>{count}</h3>
-      <button onClick={() => setCount(count + 1)}>Add</button>
+      {/* <h3>{count}</h3>
+      <button onClick={() => setCount(count + 1)}>Add</button> */}
+
+      <UseEffectFetch />
     </div>
+  );
+};
+
+const UseEffectFetch = () => {
+  const [selectedOption, setSelectedOption] = useState("films");
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const options = ["films", "people", "locations", "species", "vehicles"];
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    const fetchResults = async () => {
+      setLoading(true);
+      try {
+        let json = await (
+          await fetch(`https://ghibliapi.vercel.app/${selectedOption}`, {
+            signal,
+          })
+        ).json();
+
+        // console.log(json);
+        setResults(json);
+        setLoading(false);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchResults();
+
+    return () => controller.abort();
+  }, [selectedOption]);
+
+  const displayResults = () =>
+    results.map((i) => (
+      <div key={i.id} style={{ border: "1px solid lightblue" }}>
+        <img style={{ maxWidth: "200px" }} src={i.image} alt="poster" />
+        <h3>{i.title}</h3>
+        <h3>{i.name}</h3>
+        <h4>{i.description}</h4>
+      </div>
+    ));
+
+  return (
+    <>
+      <h2>Studio Ghibli</h2>
+
+      <select onChange={(e) => setSelectedOption(e.target.value)}>
+        {options.map((i) => (
+          <option key={i}>{i}</option>
+        ))}
+      </select>
+
+      {loading ? <h3>⌛ Loading</h3> : displayResults()}
+    </>
   );
 };
 
